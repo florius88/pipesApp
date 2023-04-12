7f63f12e3f668bb3189e0cc2aca3bf5c94f9f0c6
florius88 <florsantosbueno@gmail.com>
2023-04-12 15:57:09 +0200

    PrimeNG MenuBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 // Modulo personalizado
-import { PrimeNGModule } from './prime-ng/prime-ng.module';
+/* No se esta usando en este modulo
+import { PrimeNGModule } from './prime-ng/prime-ng.module' */
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    PrimeNGModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        /* PrimeNGModule, */
+        SharedModule
+    ]
 })
 export class AppModule { }
