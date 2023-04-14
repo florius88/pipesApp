3fb9e761814303a61c3c7d8f945b5173e9d8267d
florius88 <florsantosbueno@gmail.com>
2023-04-14 19:29:00 +0200

    Tarea pipe personalizado - VuelaPipe

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -11,6 +11,7 @@ import { NumerosComponent } from './pages/numeros/numeros.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
 import { MayusculasPipe } from './pipes/mayusculas.pipe';
+import { VuelaPipe } from './pipes/vuela.pipe';
 
 
 
@@ -20,7 +21,8 @@ import { MayusculasPipe } from './pipes/mayusculas.pipe';
     NoComunesComponent,
     NumerosComponent,
     OrdenarComponent,
-    MayusculasPipe
+    MayusculasPipe,
+    VuelaPipe
   ],
   exports: [
     BasicosComponent,
