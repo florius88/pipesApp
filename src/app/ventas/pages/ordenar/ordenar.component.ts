d44235ee48e9ae4beeba41b5b9e5a32756626447
florius88 <florsantosbueno@gmail.com>
2023-04-14 19:20:41 +0200

    PrimeTable y PrimeToolbar

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -8,6 +9,34 @@ import { Component } from '@angular/core';
 export class OrdenarComponent {
 
   enMayusculas: boolean = false;
+
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
 
   toggleMayus() {
     this.enMayusculas = !this.enMayusculas;
