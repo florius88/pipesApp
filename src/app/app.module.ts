3be5d19d82d45f102728c4fdbff416d51e01625c
florius88 <florsantosbueno@gmail.com>
2023-04-17 19:32:52 +0200

    Modulos y Rutas Version 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
-import { AppRouterModule } from './app-router.module';
+import { AppRouterModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
